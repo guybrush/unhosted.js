ce74d8fc40e6993edcb7e0ebca65ec0bd5071f88
Daniel Gröber <darklord@darkboxed.org>
2011-01-16 14:39:09 +0100

    scripts/unhosted.js is now a require.js plugin that loads the unhosted modules

--- scripts/unhosted.js
+++ scripts/unhosted.js
@@ -1,48 +1,28 @@
 /*
- * Unhosted JS library.
- *  Handles comms with unhosted storage node for unhosted web apps.
- * Copyright (C) 2010 Michiel de Jong michiel@unhosted.org
+ * The Unhosted.js library
+ * Copyright (C) 2011  Daniel Gröber <darklord ät darkboxed.org>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-define(['./crypto'
-        , './key-storage'
-        , './util'
-        , './methods'],
-function unhostedModule(crypto, keyStorage, util, methods){
-    var JSONtoCGI = util.JSONtoCGI;
-    var sendPost = util.sendPost;
-
-    var options = {
-        postURI: '/unhosted/cloudside/unhosted.php'
-        , ajaxFailure: function(){}
-    }
-
-    var Unhosted = function(user, nodeAddress){
-        // TODO better checks
-        if(!nodeAddress) {
-            throw new Error('Invalid node address');
+define(['./modules/available', './modules'], function(modules){
+    return {
+        load: function(name, req, load, config){
+            var modulePath = modules.get(name);
+            req(['./' + modulePath], function(module){
+                load(module);
+            });
         }
-
-        this.proto = 'UJ/0.2'
-        this.user = user;
-        this.address = nodeAddress;
-        this.postURI = options.postURI;
     }
-
-    Unhosted.prototype = methods;
-
-    return Unhosted;
-}); // function unhostedModule(..
+})
